8162c1ec79ef0ff67dd4df3964a9beaf61e8ff84
Sanni Suarez <sannysuarez4@gmail.com>
2023-06-20 23:17:49 -0700

    prints wvery minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * jack_bauer - prints every minute of the day of Jack Bauer
- *   * starting from 00:00 to 23:59
- *    */
+ * Jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
 void jack_bauer(void)
 {
 	int i, j;
@@ -13,7 +13,7 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i / 10) + '0');
+			-putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
