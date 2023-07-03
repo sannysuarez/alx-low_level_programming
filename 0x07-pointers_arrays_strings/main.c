f905671a94ea876b17af34fddd1f727322bc682a
Sanni Suarez <sannysuarez4@gmail.com>
2023-07-03 14:32:11 -0700

    msgs

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,7 +1,7 @@
 #ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
 
-char *_memset(char *s,char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
